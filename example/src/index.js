88c108b9629964cea3b24e3f83ffb34d6adf61e8
Arun Kumar <ak7584@gmail.com>
2018-02-17 14:48:45 +0530

    added keywords to package.json

--- example/src/index.js
+++ example/src/index.js
@@ -5,9 +5,14 @@ import { MessageManagerProvider } from 'message-manager';
 import App from './App';
 
 const elem = document.getElementById('app');
+let desktopView = true;
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  desktopView = false;
+}
 
 render(
-  <MessageManagerProvider desktopView>
+  <MessageManagerProvider desktopView={desktopView}>
     <App />
   </MessageManagerProvider>,
   elem
