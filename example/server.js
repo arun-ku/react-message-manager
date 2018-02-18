101b0c5734f6ea5cafeaec4feee885837e1020a2
Arun Kumar <ak7584@gmail.com>
2018-02-18 23:59:20 +0530

    added port from env

--- example/server.js
+++ example/server.js
@@ -9,4 +9,4 @@ app.use('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 });
 
-app.listen(3000, () => console.log('App Running'));
+app.listen(process.env.PORT || 3000, () => console.log('App Running'));
